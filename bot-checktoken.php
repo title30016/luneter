b3b65392b8c80bedc4457204e122abb5dc2bb58f
title30016 <153412570+title30016@users.noreply.github.com>
2023-12-10 12:54:44 +0700

    Update bot-checktoken.php

--- bot-checktoken.php
+++ bot-checktoken.php
@@ -1,45 +1,17 @@
 <?php
-
-
+// URL API LINE
 $API_URL = 'https://api.line.me/v2/bot/message';
 // ใส่ Channel access token (long-lived)
-$ACCESS_TOKEN = 'UOQPxijn7L+I28q0MwwEIFWIhU6UZM+qb+8d/h4ZlW0DgJNgbPzP7rrSgfwnJBY8GW+dJGk4BQxRdXAHSZQrwmH/+3146ZjSubnSl6kfr+nKuxyu/+x3UdUSS3yj+x+KI/x/2H8nf2LhzQfxDaoB5QdB04t89/1O/w1cDnyilFU=';
+$ACCESS_TOKEN = '##########################';
 // ใส่ Channel Secret
-$CHANNEL_SECRET = '2f36b9046076072402d25108ee12b067';
+$CHANNEL_SECRET = '##########################';
 
-
+// Set HEADER
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
-
-$request = file_get_contents('php://input');   // Get request content
-$request_array = json_decode($request, true);   // Decode JSON to Array
-
-
-
-if ( sizeof($request_array['events']) > 0 ) {
-
-    foreach ($request_array['events'] as $event) {
-
-        $reply_message = '';
-        $reply_token = $event['replyToken'];
-
-
-        $data = [
-            'replyToken' => $reply_token,
-            'messages' => [['type' => 'text', 'text' => json_encode($request_array)]]
-        ];
-        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-        echo "Result: ".$send_result."\r\n";
-        
-    }
-}
-
-echo "OK";
-
-
-
+// Get request content
+$request = file_get_contents('php://input');
+// Decode JSON to Array
+$request_array = json_decode($request, true);
 
 function send_reply_message($url, $post_header, $post_body)
 {
@@ -55,4 +27,22 @@ function send_reply_message($url, $post_header, $post_body)
     return $result;
 }
 
+if ( sizeof($request_array['events']) > 0 ) {
+      foreach ($request_array['events'] as $event) {
+      
+      $reply_message = '';
+      $reply_token = $event['replyToken'];
+      $data = [
+         'replyToken' => $reply_token,
+         'messages' => [
+            ['type' => 'text', 
+             'text' => json_encode($request_array)]
+         ]
+      ];
+      $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+      $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+      echo "Result: ".$send_result."\r\n";
+   }
+}
+echo "OK";
 ?>
